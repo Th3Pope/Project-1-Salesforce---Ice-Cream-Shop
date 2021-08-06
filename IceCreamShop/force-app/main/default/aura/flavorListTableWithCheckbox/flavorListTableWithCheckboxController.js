c25530fed699fc0f506bba4dad184118f27c3381
Th3Pope <48426524+Th3Pope@users.noreply.github.com>
2021-08-06 11:56:32 -0700

    data pulled from UI

--- IceCreamShop/force-app/main/default/aura/flavorListTableWithCheckbox/flavorListTableWithCheckboxController.js
+++ IceCreamShop/force-app/main/default/aura/flavorListTableWithCheckbox/flavorListTableWithCheckboxController.js
@@ -1,22 +1,27 @@
 ({
     init: function (cmp, event, helper) 
 	{
-
 		helper.getFlavorRecordsAndSetToTable(cmp);
-		
 	},
 
-	getSelectedFlavor: function (cmp, event) 
+	getSelectedFlavor: function (cmp, event, helper) 
 	{
-		var selectedFlavorRow = event.getParam('selectedRows');
+		//var selectedFlavorRow = event.getParam('selectedRows');
 		// Display that fieldName of the selected rows
-		for (var i = 0; i < selectedFlavorRow.length; i++)
-		{
-			alert("You selected: " + selectedFlavorRow[0].Name);
-		}
+		//for (var i = 0; i < selectedFlavorRow.length; i++)
+		//{
+		//	alert("You selected: " + selectedFlavorRow[0].Name + selectedFlavorRow[0].Flavor_Index__c);
+		//}
+		helper.setSelectedFlavor(event.getParam('selectedRows')); 
 	},
 
-	onChange: function (cmp, evt, helper) {
-        alert(cmp.find('select').get('v.value') + ' pie is good.');
-    }	
+	selectedScoopNum: function (cmp, evt, helper) 
+	{
+		helper.setScoopNumber(cmp.find('select').get('v.value'));
+    },
+
+	buttonClickHandler : function(cmp, event, helper)
+	{
+		helper.createRecord(); 
+	}
 })
--- IceCreamShop/force-app/main/default/aura/flavorListTableWithCheckbox/flavorListTableWithCheckboxHelper.js
+++ IceCreamShop/force-app/main/default/aura/flavorListTableWithCheckbox/flavorListTableWithCheckboxHelper.js
@@ -1,12 +1,48 @@
 ({
+    scoopNumber: 0,
+    flavorName: '',
+    flavorNumSelected: 0,
 
-    getFlavorRecordsAndSetToTable : function(component) {
+    setScoopNumber : function(num)
+    {
+        if(num != '')
+        {
+            this.scoopNumber = num;
+        }          
+    },
+
+    setSelectedFlavor : function(flavor)
+    {
+        if(flavor != '')
+        {
+            this.flavorName = flavor; 
+            this.flavorNumSelected = flavor.length;     
+        }
+    },
+
+    createRecord : function()
+    {
+        if(this.scoopNumber > 0 && this.flavorName != '' 
+            && this.flavorNumSelected == 1)
+        {
+            //call apex to create record
+        }else
+        {
+            alert('Only one flavor may selected, number of scoops must be 1 or greater');
+        }
+    },
+
+    
+
+    getFlavorRecordsAndSetToTable : function(component) 
+    {
 
         component.set('v.colLabels', [
             {label: 'Flavor Name', fieldName: 'Name', type: 'text'},
                 {label: 'Price Per Scoop', fieldName: 'Sale_Price_Per_Scoop__c', type: 'currency'}
             ]);
-        var action = component.get("c.getFlavorsForAura");
+        component.set('v.id', [{fieldName: 'Flavor_Index__c'}]);
+        let action = component.get("c.getFlavorsForAura");
         action.setParams({
         });
 
