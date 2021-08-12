6122c2d23cda0e865a82aae8c6a9675fc3bf99ea
Theodore Mitchell <softwaretheodore@gmail.com>
2021-08-11 20:19:28 -0500

    Contact Us improved

--- IceCreamShop/force-app/main/default/aura/ContactUsComponent/ContactUsComponentController.js
+++ IceCreamShop/force-app/main/default/aura/ContactUsComponent/ContactUsComponentController.js
@@ -21,15 +21,21 @@
         
         let createNewCase = component.get('c.createNewCase');
         createNewCase.setParams({firstName:component.find("firstNameIn").get('v.value'),
-                                      lastName:component.find("lastNameIn").get('v.value'),
-                                      email:component.find("emailIn").get('v.value'),
-                                      message:component.find("messageIn").get('v.value')});
+                                 lastName:component.find("lastNameIn").get('v.value'),
+                                 email:component.find("emailIn").get('v.value'),
+                                 subject:component.find("subjectIn").get('v.value'),
+                                 message:component.find("messageIn").get('v.value')});
         createNewCase.setCallback(this, function(response) {
             let returnString = response.getReturnValue();
             if (returnString === 'Success') {
-                alert('Message submitted!');
+                alert('Message submitted!  We will get back to you as soon as possible.');
+                component.find("firstNameIn").set("v.value", "");
+                component.find("lastNameIn").set("v.value", "");
+                component.find("emailIn").set("v.value", "");
+                component.find("subjectIn").set("v.value", "");
+                component.find("messageIn").set("v.value", "");
             } else {
-            	alert(returnString);
+                alert(returnString);
             }
         });
         
